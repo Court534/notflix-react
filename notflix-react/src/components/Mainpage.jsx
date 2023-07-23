f56f98f25650019b0b48ea4e31f733dd05175ed3
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-23 17:59:40 +0100

    Started styling the mainpage.jsx

--- notflix-react/src/components/Mainpage.jsx
+++ notflix-react/src/components/Mainpage.jsx
@@ -6,15 +6,21 @@ import axios from 'axios'
 const Mainpage = () => {
   const [movies, setMovies] = useState([])
 
+  const movie = movies[Math.floor(Math.random() * movies.length)]
+
   useEffect(() => {
     axios.get(requests.requestPopular).then((res) => {
-      setMovies(res.data)
+      setMovies(res.data.results)
     })
   }, [])
   console.log(movies);
 
   return (
-    <div>Mainpage</div>
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+      </div>
+    </div>
   )
 }
 
