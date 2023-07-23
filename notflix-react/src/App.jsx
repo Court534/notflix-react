16e7bc4039cc5a85e575d716265f56f593f9965e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-23 16:44:53 +0100

    Created .env and updated gitignore

--- notflix-react/src/App.jsx
+++ notflix-react/src/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar"
-
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+       <Route path='/' element={<Home />} />
+      </Routes>
+
+    </>
   )
 }
 
---
+++ notflix-react/src/components/Mainpage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+const Mainpage = () => {
+  const [movies, setMovies] = useState([])
+
+  return (
+    <div>Mainpage</div>
+  )
+}
+
+export default Mainpage
