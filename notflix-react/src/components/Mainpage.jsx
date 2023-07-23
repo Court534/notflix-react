641095eb58bd1cd7858a3d27b3d7584a1c6bf116
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-23 17:05:37 +0100

    Used axios to call the moviedb API

--- notflix-react/src/components/Mainpage.jsx
+++ notflix-react/src/components/Mainpage.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import requests from "../requests"
+import axios from 'axios'
 
 
 const Mainpage = () => {
   const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((res) => {
+      setMovies(res.data)
+    })
+  }, [])
+  console.log(movies);
 
   return (
     <div>Mainpage</div>
--- notflix-react/src/pages/Home.jsx
+++ notflix-react/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Mainpage from '../components/Mainpage'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Mainpage />
+    </>
   )
 }
 
