b18e929176ae9fe4c81d0f454c86a8cb6122eed6
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-24 14:26:37 +0100

    Started styling the rows

--- notflix-react/src/components/Row.jsx
+++ notflix-react/src/components/Row.jsx
@@ -1,29 +1,31 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react'
-import Movie from './Movie';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
-
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
-      setMovies(response.data.results)
-    })
-  }, [fetchURL])
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
 
   return (
     <>
-      <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-      <div className='relative flex items-center'>
-        <div id={"slider"}>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+          id={"slider"}
+        >
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
