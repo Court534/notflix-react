a402f988177b6315dee9ea9583f8e6082ba159a4
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-24 15:17:03 +0100

    App is finished, now moving onto the firebase setup

--- notflix-react/src/pages/Home.jsx
+++ notflix-react/src/pages/Home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <>
       <Mainpage />
-      <Row title="UpComing" fetchURL={requests.requestUpcoming} />
-      <Row title="Trending" fetchURL={requests.requestTrending} />
-      <Row title="TopRated" fetchURL={requests.requestTopRated} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID='1' title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row rowID='2' title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID='3' title="TopRated" fetchURL={requests.requestTopRated} />
+      <Row rowID='4' title="Popular" fetchURL={requests.requestPopular} />
     </>
   )
 }
