a78a07d40ca6c079b8a743804171338956a91836
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-25 14:51:56 +0100

    Started connection the firebase back end

--- notflix-react/src/components/Navbar.jsx
+++ notflix-react/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/authContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  console.log(user);
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
@@ -8,16 +23,30 @@ const Navbar = () => {
           NOTFLIX
         </h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="pr-4 cursor-pointer text-white">Sign In</button>
-        </Link>
-        <Link to='/signup'>
-          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-            Sign Up
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="pr-4 cursor-pointer text-white">Account</button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
+          >
+            Logout
           </button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="pr-4 cursor-pointer text-white">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
