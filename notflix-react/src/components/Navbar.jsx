d399478ee472b7cdb230a926eae72acbc3ca2994
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-07-26 14:10:46 +0100

    Finished the final styling

--- notflix-react/src/components/Navbar.jsx
+++ notflix-react/src/components/Navbar.jsx
@@ -17,28 +17,28 @@ const Navbar = () => {
   };
 
   return (
-    <div className="sm:flex justify-between z-[100] p-4 w-full absolute">
-      <Link to="/">
-        <h1 className="text-red-600 text-5xl font-bold cursor-pointer mb-4">
+    <div className="flex flex-wrap justify-between items-center z-[100] p-4 w-full absolute">
+      <Link to="/" className="mb-4 sm:mb-0">
+        <h1 className="text-red-600 text-5xl font-bold cursor-pointer">
           NOTFLIX
         </h1>
       </Link>
       {user?.email ? (
-        <div className="">
+        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
           <Link to="/account">
-            <button className="pr-4 cursor-pointer text-white">Account</button>
+            <button className="cursor-pointer text-white ml-4 sm:mt-2">Account</button>
           </Link>
           <button
             onClick={handleLogout}
-            className="bg-red-600 sm:px-6 sm:py-2 rounded cursor-pointer text-white"
+            className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white"
           >
             Logout
           </button>
         </div>
       ) : (
-        <div>
+        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
           <Link to="/login">
-            <button className="pr-4 cursor-pointer text-white">Sign In</button>
+            <button className="cursor-pointer text-white ml-6 sm:mt-2">Sign In</button>
           </Link>
           <Link to="/signup">
             <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
